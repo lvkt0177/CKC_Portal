881977408a2454bde3807890b5e72b07e8cb77e4
lvkt0177 <lvkt0177@gmail.com>
2025-06-03 12:00:11 +0700

    3-6: Cap nhat code

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\Admin\GiangVienController;
 
-Route::get('/user', function () {
-    return response()->json(['message' => 'pong']);
-});
-
 
 Route::resource('giangvien', GiangVienController::class)->only(['index', 'show']);
